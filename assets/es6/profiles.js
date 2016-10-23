195f61316fb1a7691dad296c94b91ee437307a1a
giTomas <tomas.kosegi@gmail.com>
2016-10-24 00:39:42 +0200

    add layers of layout

--- assets/es6/profiles.js
+++ assets/es6/profiles.js
@@ -1,55 +1,4 @@
 
-  const profiles = [
-    {
-       id: 'maros-ondrejka',
-       name: 'Maroš Ondrejka',
-       text: 'Has facer ornatus ea, ex stet meis fuisset pro, mel vitae euismod an. Te veritus iracundia hendrerit vel, ea sea tractatos definitionem. Id mea exerci consequat gloriatur, in liber vocent tacimates vim. Exerci referrentur cum ut, duo ne quod altera. Quot singulis cum id. Nam id numquam facilisi consequuntur, ut eam oratio referrentur, recteque incorrupte has te. Viris tantas torquatos ut eam, ea usu melius copiosae imperdiet.Cu pro causae audiam minimum, ipsum vivendo id est. Paulo commodo nonumes sed ea, ut sit aliquip posidonium. Brute offendit consequat sed cu. Quando soleat disputando in duo, ex mazim civibus theophrastus pro. Libris iisque ex nec, quo quaeque aliquid torquatos id, duo dolore partem no. Eos esse definiebas ut, no sumo nusquam pertinacia mea, cu inani commodo deleniti ius.'
-    }, {
-       id: 'albert-russ',
-       name: 'Albert Russ',
-       text: 'Omnesque intellegebat conclusionemque pri ex, id nec veniam ubique. Harum soleat neglegentur mea in, ei has natum quando. Sumo altera est et, utamur utroque recteque ut vis. Falli salutandi ius in, pro no minimum elaboraret. Mei ipsum legere repudiandae in, mei ea reque elitr.Cu pro causae audiam minimum, ipsum vivendo id est. Paulo commodo nonumes sed ea, ut sit aliquip posidonium. Brute offendit consequat sed cu. Quando soleat disputando in duo, ex mazim civibus theophrastus pro. Libris iisque ex nec, quo quaeque aliquid torquatos id, duo dolore partem no. Eos esse definiebas ut, no sumo nusquam pertinacia mea, cu inani commodo deleniti ius.',
-    }, {
-       id: 'tomas-kosegi',
-       name: 'Tomáš Kösegi',
-       text: 'Has facer ornatus ea, ex stet meis fuisset pro, mel vitae euismod an. Te veritus iracundia hendrerit vel, ea sea tractatos definitionem. Id mea exerci consequat gloriatur, in liber vocent tacimates vim. Exerci referrentur cum ut, duo ne quod altera. Quot singulis cum id. Nam id numquam facilisi consequuntur, ut eam oratio referrentur, recteque incorrupte has te. Viris tantas torquatos ut eam, ea usu melius copiosae imperdiet.Cu pro causae audiam minimum, ipsum vivendo id est. Paulo commodo nonumes sed ea, ut sit aliquip posidonium. Brute offendit consequat sed cu. Quando soleat disputando in duo, ex mazim civibus theophrastus pro. Libris iisque ex nec, quo quaeque aliquid torquatos id, duo dolore partem no. Eos esse definiebas ut, no sumo nusquam pertinacia mea, cu inani commodo deleniti ius.'
-    }
-  ];
-
-  function createMarkup(obj) {
-    if (!isObject(obj)) {
-        console.log('Invalid object');
-        return obj;
-    }
-
-    const {id, name, text} = obj;
-    const img = 'images/' + id + '.jpg';
-
-    return `
-          <div id='js-profile' class='c-profile'>
-              <div class='l-col-1-2'>
-                <figure id='js-profile-figure' class=c-profile__figure>
-                  <img src='${img}' alt='${name}'>
-                </figure>
-                  <h2 id='js-profile-name' class='c-profile__name'>
-                    ${name}
-                  </h2>
-              </div>
-              <div class='l-col-2-2'>
-                <article id='js-profile-text' class='c-profile__text'>
-                  <p>${text}</p>
-                </article>
-              </div>
-              <div id='js-modal-close' class='c-modal__close'>
-                <svg fill="#000000" height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg">
-                  <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
-                  <path d="M0 0h24v24H0z" fill="none"></path>
-                </svg>
-              </div>
-          </div>
-          `;
-  };
-
-
   function animation_1(modal){
       const tl = new TimelineLite();
       tl.to(modal, 0.1, {display: 'block'})
@@ -63,9 +12,8 @@
       }, false);
   }
 
-  function animation_2(modal, src, tmp){
+  function animation_2(modal, src){
     const tl = new TimelineLite();
-    modal.innerHTML = tmp;
     src.classList.add('member-is-clicked');
 
     tl.to(modal, 0.1, {display: 'block'})
@@ -84,7 +32,8 @@
   }
 
   function addModal(el, tmp, anim) {
-    if (tmp === 'undefined') {
+    if (typeof tmp === 'undefined') {
+      console.log(tmp);
       return;
     }
     const modal = document.getElementById(el);
@@ -94,28 +43,33 @@
 
   function addModal2(el, tmp, anim, src) {
     if (typeof tmp === 'undefined') {
+      console.log(tmp);
       return;
     }
     const modal = document.getElementById(el);
-    return anim(modal, src, tmp);
+    modal.innerHTML = tmp;
+    return anim(modal, src);
   };
 
   function profileHandler(e) {   //promise???
-    const profile = findData(profiles, 'id', e.currentTarget.dataset.member);
-    const markup = createMarkup(profile);
+    const profile = findData(data.profiles, 'id', e.currentTarget.dataset.id);
+    const markup = Tmps.markupProfiles(profile);
     addModal('js-modal', markup, animation_1);
   }
 
   function profileHandler_2(e) {   //promise???
     const src = e.currentTarget;
     // const profile = findValueByIndex(profiles, 'id', src.dataset.member);
-    const profile = findValue(profiles, 'id', src.dataset.member);
-    const markup = createMarkup(profile);
+    const profile = findValue(data.profiles, 'id', src.dataset.id);
+    const markup = Tmps.markupProfiles(profile);
+
     addModal2('js-modal', markup, animation_2, src);
   }
 
   const members = document.querySelectorAll('.js-member');
   const members_2 = document.querySelectorAll('.js-member-anim');
+  const modal = document.querySelectorAll('.js-modal');
 
   forEachEl(members, profileHandler);
   forEachEl(members_2, profileHandler_2);
+  forEachEl(modal, profileHandler_2);
---
+++ assets/es6/tmps.js
@@ -0,0 +1,39 @@
+const Tmps = (function(){
+
+const _markupProfiles = (obj) => {
+  if (!isObject(obj)) {
+      console.log('Invalid object');
+      return obj;
+  }
+  //const {id, name, text} = obj;
+  // const img = 'images/' + id + '.jpg';
+  return `
+        <div id='js-profile' class='c-profile'>
+            <div class='l-col-1-2'>
+              <figure id='js-profile-figure' class=c-profile__figure>
+                <img src='images/${obj.id}.jpg' alt='${obj.name}'>
+              </figure>
+                <h2 id='js-profile-name' class='c-profile__name'>
+                  ${obj.name}
+                </h2>
+            </div>
+            <div class='l-col-2-2'>
+              <article id='js-profile-text' class='c-profile__text'>
+                <p>${obj.text}</p>
+              </article>
+            </div>
+            <div id='js-modal-close' class='c-modal__close'>
+              <svg fill="#000000" height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg">
+                <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"></path>
+                <path d="M0 0h24v24H0z" fill="none"></path>
+              </svg>
+            </div>
+        </div>
+        `;
+};
+
+return {
+  markupProfiles: _markupProfiles
+};
+
+})();
